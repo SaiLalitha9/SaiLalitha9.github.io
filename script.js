4e372081acef34474676af97bc1b3604c9559efe
SaiLalitha9 <104979360+SaiLalitha9@users.noreply.github.com>
2025-03-23 21:14:56 -0500

    working url

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const works = {
-    "task1" : {"title" : "Project 1", "url" : "works/task1/index.html"},
-    "task2" : {"title" : "Project 2", "url" : "works/task2/index.html"},
-    "task3" : {"title" : "Project 3", "url" : "works/task3/index.html"},
+    "task1" : {"title": "Project 1", "url" : "Project/Frontend/headings.html"},
+    "task2" : {"title": "Project 2", "url" : "Project/Frontend/displaybox2.html"},
+    "task3" : {"title": "Project 3", "url" : "Project/Frontend/Midterm.html"},
+    "task4" : {"title": "Project 4", "url" : "Project/Frontend/Task5.html"},
+    "task5" : {"title": "Project 5", "url" : "Project/Frontend/Task7.html"},
+    "task6" : {"title": "Project 6", "url" : "Project/Frontend/Task8.html"},
+    "task7" : {"title": "Project 7", "url" : "Project/Frontend/Task9.html"},
+    "task8" : {"title": "Project 8", "url" : "Project/Frontend/Task10.html"},
+    "task9" : {"title": "Project 9", "url" : "Project/Frontend/Task11.html"},
+    "task10" : {"title": "Project 10", "url" : "Project/Frontend/Task12.html"},
+    "task11" : {"title": "Project 11", "url" : "Project/Frontend/Task13.html"},
+    "task12" : {"title": "Project 12", "url" : "Project/Frontend/Task14.html"},
+    "task13" : {"title": "Project 13", "url" : "Project/Frontend/Task15.html"},
+    "task14" : {"title": "Project 14", "url" : "Project/Frontend/Task15(1).html"},
+    "task15" : {"title": "Project 15", "url" : "Project/Frontend/Task15(2).html"},
+    "task16" : {"title": "Project 16", "url" : "Project/Frontend/Task15(3).html"},
 };
 
 // write your code here 
